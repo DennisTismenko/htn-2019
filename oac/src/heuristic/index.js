dade66bfdfc0e4daf73d320eaeaa3c3ddbbc337a
Dennis Tismenko <dtismenkodeveloper@gmail.com>
2019-09-14 18:49:43 -0400

    Finish implementation of security heuristics

--- oac/src/heuristic/index.js
+++ oac/src/heuristic/index.js
@@ -10,7 +10,7 @@ module.exports = async function computeHeuristics(context) {
 }
 
 // {
-//   severity: "high"|"medium"|"low",
+//   severity: "high"|"medium"|"low"|"none",
 //   category: "quality",
 //   reference: "{\"eslint/no-debugger\":\"src/index.js:13:34\"}",
 //   message: "Uses debugger statement",
--- oac/src/heuristic/snyk.js
+++ oac/src/heuristic/snyk.js
@@ -1,8 +1,14 @@
 const snyk = require('snyk');
+const category = 'security';
 
 module.exports = async function snykHeuristics(context) {
     let pkgName = context.pkg.name;
     snyk.api = process.env.SNYK_TOKEN;
     const result = await snyk.test(pkgName);
-    console.log(result);
+    return result.vulnerabilities.map((vuln) => ({
+        severity: vuln.severity,
+        category,
+        reference: JSON.stringify({ snyk: vuln.id }),
+        message: vuln.description,
+    }));
 }
