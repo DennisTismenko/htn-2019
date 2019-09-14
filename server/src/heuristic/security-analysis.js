d200773a00774ebc4a4ab0a1978e9070b066acd4
Dennis Tismenko <dtismenkodeveloper@gmail.com>
2019-09-14 17:41:25 -0400

    Remove refactored files

--- server/src/heuristic/security-analysis.js
+++
@@ -1,8 +0,0 @@
-// const httpReq = require('../util/http-req');
-const REGISTRY_BASE_URL = 'https://registry.npmjs.org/';
-
-module.exports = {
-    analyze(npmPackage) {
-        httpReq.get(`${REGISTRY_BASE_URL}/${npmPackage.packageName}`);
-    }
-} 
