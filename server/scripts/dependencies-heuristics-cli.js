95783f46bbfb59ce6d4b5f32dbd83cd44515e1ac
Maxim Mikhaylov <lawnboymadmax@gmail.com>
2019-09-15 08:27:33 -0400

    ./scripts/dependencies-heuristics-cli.js body-parser 1.19.0

---
+++ server/scripts/dependencies-heuristics-cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+//const main = require('../src/data-pipeline');
+const main = require('../src/service/heuristic-service');
+
+function usage() {
+  const prog = process.argv[1];
+  console.log(`usage: ${prog} <pkg> <version> [directory]`)
+}
+
+if (require.main === module) {
+  const [pkg, version, directory] = process.argv.slice(2);
+  if (pkg == null || version == null) {
+    usage();
+    process.exit(1);
+  }
+
+  main.runDependenciesHeuristics(pkg, version, directory)
+    .then((result) => {
+      console.log(result);
+      process.exit(0);
+    })
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
--- server/src/service/heuristic-service.js
+++ server/src/service/heuristic-service.js
@@ -1,5 +1,8 @@
 const dataAnalyzer = require('../data-pipeline');
 const dbService = require('./database-service');
+const npmRegistry = require('../data-pipeline/utility/npmRegistry');
+const semver = require('semver');
+const debug = require('debug')('main');
 
 module.exports = {
     async runHeuristics(pkgName, version) {
@@ -12,4 +15,47 @@ module.exports = {
           return pkg.heuristics
         }
     },
+    async runDependenciesHeuristics(pkgName, version) {
+        let result = [];
+        queue = [[pkgName, version]]; // start BFS at source
+        seenPackages = new Set();
+        while (queue.length > 0) {
+            let u = queue.shift();
+            let name = u[0], version = u[1];
+            console.log('Dequeued: ', name, version);
+            let pkgInfo = await npmRegistry(name, version);
+            for (let [dependencyName, range] of Object.entries(pkgInfo.dependencies)) {
+                console.log(dependencyName, range);
+                console.log('To enqueue ', name, version);
+                let info = await npmRegistry(dependencyName, '');
+                let versions = Object.keys(info.versions);
+                let dependencyVersion = semver.maxSatisfying(versions, range);
+                let dependency = [dependencyName, dependencyVersion];
+                if (!seenPackages.has(dependency)) { // don't search duplicates
+                    queue.push(dependency);
+                    seenPackages.add(dependency);
+                }
+                console.log('queue: ' + queue);
+                let val = '', item;
+                for (item of seenPackages.values())
+                    val += item + ', ';
+                console.log('seenPackages: ' + val);
+            }
+            let pkg;
+            for (pkg of seenPackages) {
+                let [name, version] = pkg;
+                pkg = await dbService.getPackageByNameVersion(name, version);
+                if (!pkg) {
+                    pkg = await dataAnalyzer(pkgName, version);
+                    await dbService.createPackage(pkgName, version, pkg);
+                    result.push(pkg);
+                } else {
+                    result(pkg.heuristics);
+                }
+            }
+        }
+        return result 
+      
+    },
+        
 }
